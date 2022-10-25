c0d3b416aebc8f1e9d3955d34935900591b6a0f1
cristianocorrea3 <94192765+cristianocorreamoraes@users.noreply.github.com>
2022-10-25 13:25:57 -0300

    Teste
    teste

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const controllerTeste = require('./controller/controllerTeste');
 
 app.use('/', controllerTeste);
 
-app.listen(3000, ()=>{
+const port = process.env.PORT || 3000
+
+app.listen(port, ()=>{
     console.log('SERVIDOR RODANDO EM - http://localhost:3000');
 });
