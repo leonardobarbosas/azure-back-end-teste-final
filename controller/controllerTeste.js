1009e23a6b8417d02a1ba4cd2bc62af47f6ed681
cristianocorrea3 <94192765+cristianocorreamoraes@users.noreply.github.com>
2022-10-25 13:34:30 -0300

    Teste-2
    Teste-2

--- controller/controllerTeste.js
+++ controller/controllerTeste.js
@@ -4,25 +4,25 @@ const router = express.Router();
 
 router.get('/read', (req, res)=>{
     res.status(200).json(
-        {msg:"ROTA DE LEITURA DE DADOS"} 
+        {msg:"DEPLOY AZURE: ROTA DE LEITURA DE DADOS"} 
     );
 });
 
 router.post('/create', (req, res)=>{
     res.status(200).json(
-        {msg:"ROTA DE GRAVAÇÃO DE DADOS"} 
+        {msg:"DEPLOY AZURE: ROTA DE GRAVAÇÃO DE DADOS"} 
     );
 });
 
 router.put('/update', (req, res)=>{
     res.status(200).json(
-        {msg:"ROTA DE ATUALIZAÇÃO DE DADOS"} 
+        {msg:"DEPLOY AZURE: ROTA DE ATUALIZAÇÃO DE DADOS"} 
     );
 });
 
 router.delete('/delete', (req, res)=>{
     res.status(200).json(
-        {msg:"ROTA DE EXCLUSÃO DE DADOS"} 
+        {msg:"DEPLOY AZURE: ROTA DE EXCLUSÃO DE DADOS"} 
     );
 });
 
